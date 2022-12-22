260a6d125975a134bfc0c3ca6795a002a741c760
Adrian Arco <a.arco@fruntera.com>
2022-12-22 10:44:11 +0100

    refactor pokemon card to externalize getpictureColor function

--- src/components/Pokemon/Pokemon.tsx
+++ src/components/Pokemon/Pokemon.tsx
@@ -1,39 +1,44 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, Image} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {SimplePokemon} from '../../interfaces/interfaces';
 import {FadeInImage} from '../FadeInImage/FadeInImage';
 import pokemonStyles from './pokemonStyle';
-import {useEffect} from 'react';
-import ImageColors from 'react-native-image-colors';
-import {IOSImageColors} from 'react-native-image-colors/lib/typescript/types';
+
+import getPicturecolor from '../../utils/getPictureColor';
 
 interface PokemonProps {
     pokemon: SimplePokemon;
 }
-const Pokemon = ({pokemon}: PokemonProps) => {
+export const Pokemon = ({pokemon}: PokemonProps) => {
     const [backgroundColor, setBackgroundColor] = useState('grey');
 
     useEffect(() => {
-        ImageColors.getColors(pokemon.picture, {
-            fallback: 'grey',
-        }).then(colors => {
-            colors.platform === 'android'
-                ? setBackgroundColor(colors.dominant || 'grey')
-                : setBackgroundColor(
-                      (colors as IOSImageColors).background || 'grey',
-                  );
-        });
+        (async () => {
+            const pokemonColor = await getPicturecolor(pokemon.picture);
+            if (pokemonColor.platform === 'android') {
+                setBackgroundColor(pokemonColor.dominant || 'grey');
+            } else if (pokemonColor.platform === 'ios') {
+                setBackgroundColor(pokemonColor.background);
+            } else {
+                setBackgroundColor('grey');
+            }
+        })();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
         <TouchableOpacity activeOpacity={0.8}>
             <View style={{...pokemonStyles.cardContainer, backgroundColor}}>
-                <Text style={pokemonStyles.title}>{pokemon.name}</Text>
-                <Text style={pokemonStyles.title}>#{pokemon.id}</Text>
+                <Text data-testID="pokemonTitle" style={pokemonStyles.title}>
+                    {pokemon.name}
+                </Text>
+                <Text data-testID="pokemonId" style={pokemonStyles.title}>
+                    #{pokemon.id}
+                </Text>
                 <View style={pokemonStyles.pokeballContainer}>
                     <Image
+                        data-testID="imagePokemon"
                         source={require('../../assets/pokebola-blanca.png')}
                         style={pokemonStyles.pokeball}
                     />
@@ -46,5 +51,3 @@ const Pokemon = ({pokemon}: PokemonProps) => {
         </TouchableOpacity>
     );
 };
-
-export default Pokemon;
